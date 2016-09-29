cf4c9127cfc542f5e2867fd1096a1ec13c697eb3
annette-arrigucci <annette.arrigucci@gmail.com>
2016-09-29 15:50:55 -0400

    added script files

---
+++ Services/CarServices.js
@@ -0,0 +1,43 @@
+﻿var baseUrl = 'http://aarrigucci-carfinder.azurewebsites.net';
+
+app.service('svcAppCar', ['$http', function ($http) {
+    this.HCLYears = function () {
+        return $http.get(baseUrl + '/api/Cars/Years')
+            .then(function (response) { return response.data; });
+    };
+
+    this.HCLMakes = function (year) {
+        var options = { params: { year: year } };
+        return $http.get(baseUrl + '/api/Cars/Makes', options)
+            .then(function (response) { return response.data; });
+    };
+
+    this.HCLModels = function (year, make) {
+        var options = { params: { year: year, make: make } };
+        return $http.get(baseUrl + '/api/Cars/Models', options)
+            .then(function (response) { return response.data; });
+    };
+
+    this.HCLTrims = function (year, make, model) {
+        var options = { params: { year: year, make: make, model: model } };
+        return $http.get(baseUrl + '/api/Cars/Trims', options)
+            .then(function (response) { return response.data; });
+    };
+
+    this.HCLCars = function (year, make, model, trim) {
+        var options = { params: { year: year, make: make, model: model, trim: trim } };
+        return $http.get(baseUrl + '/api/Cars/Car', options)
+            .then(function (response) {
+                return response.data;
+            });
+    };
+
+    this.HCLCar = function (year, make, model, trim) {
+        var options = { params: { year: year, make: make, model: model, trim: trim } };
+        return $http.get(baseUrl + '/api/Cars/Car', options)
+            .then(function (response) {
+                return response.data;
+            });
+    };
+
+}]);
